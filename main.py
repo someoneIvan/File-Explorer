f9ba99610b98dc047cffbd8b50f20e3fc80e4e56
pleseinovivan <pleseinovivan@gmail.com>
2026-04-13 22:13:56 +0300

    Ну короче много всякого

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from core.scan import scan
-from core.action import change_directory
+from core.action import change_directory, user_help
 
 from pathlib import Path
-import shutil
+import shutil, os
 
 if __name__ == "__main__":
     print("Welcome to File Explorer!\n")
     while True:
-        print(f"{scan(Path.home)}\nВведите команду:")
+        p = Path.home
+        print(f"{scan(p)}\n")
+        choise = input("Введите команду:")
+
+        try:
+            if choise == "help":
+                user_help()
+            elif choise[0] == "cd":
+                p = change_directory(p, choise[:0])
+                print(scan(p))
+            elif choise == "list":
+                print(scan(p))
+            elif choise[0] = "del":
+                try:
+                    os.remove(os.path.join(p, choise[:0]))
+                except PermissionError or IsADirectoryError:
+                    shutil.rmtree(os.path.join(p, choise[:0]))
+            elif
